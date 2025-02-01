1bebabccac232a516cf0d59020ce8f4630b5fc22
Jason Gao <gaojason@usc.edu>
2025-02-01 05:25:47 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -5,9 +5,31 @@
 
 #include "ulliststr.h"
 
+using namespace std;
+
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+  ULListStr list;
+
+  cout << "is list empty? " << list.empty() << endl;
+  cout << "list size is " << list.size() << endl;
+  list.push_back("one");
+  cout << "First pushback, back: " << list.back() << endl;
+  list.push_back("two");
+  cout << "Second pushback, back: " << list.back() << endl;
+  cout << "Second pushback, front: " << list.front() << endl;
+  cout << "Second pushback, size: " << list.size() << endl;
+
+  list.push_front("three");
+  list.push_front("four");
+  cout << "Pushfront, front: " << list.front() << endl;
+  cout << "Pushfront, size: " << list.size() << endl;
+
+  list.pop_back();
+  list.pop_front();
+  cout << "Pop, front: " << list.front() << endl;
+  cout << "Pop, back: " << list.back() << endl;
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,108 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val){ // O(1)
+  if(tail_ == NULL || tail_->last >= ARRSIZE){ // when list is empty or list overflows
+    Item* newNode = new Item();
+    newNode->prev = tail_; // link to previous node
+    if(tail_ != NULL){ 
+      tail_->next = newNode; // link previous to this
+    } else {
+      head_ = newNode; // when list is empty ...
+    }
+    tail_ = newNode; 
+  }
+
+  // set value
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  size_++;
+}
+
+void ULListStr::pop_back(){
+  if(empty()){
+    return;
+  }
+  tail_->last--; // move 'last' backward to pop
+  size_--;
+
+  if(tail_->first == tail_->last){ // check if list is empty->delete it
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    if(tail_){
+      tail_->next = NULL;
+    } else {
+      head_ = NULL;
+    }
+    delete temp;
+  } 
+}
+
+void ULListStr::push_front(const std::string& val){
+  if(head_ == NULL || head_->first == 0){ // when list empty or overflows
+    Item* newNode = new Item();
+    newNode->next = head_; // link this to previous
+    newNode->first = ARRSIZE;
+    newNode->last = ARRSIZE;
+    if(head_ != NULL){
+      head_->prev = newNode; // link previous to this
+    } else {
+      tail_ = newNode; // when empty
+    }
+    head_ = newNode;
+  }
+  
+  // set value
+  head_->first--;
+  head_->val[head_->first] = val;
+  size_++;
+}
+
+void ULListStr::pop_front(){
+  if(empty()){
+    return;
+  }
+  head_->first++; // move 'first' forward
+  size_--;
+
+  if(head_->first == head_->last){ // check if list is empty->delete it
+    Item* temp = head_;
+    head_ = head_->next;
+    if(head_){
+      head_->prev = NULL;
+    } else {
+      tail_ = NULL;
+    }
+    delete temp;
+  }
+}
+
+std::string const & ULListStr::back() const { 
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const { // O(1)
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const { // O(n)
+  if(loc >= size_){
+    return NULL; // return null when non-existent loc provided
+  }
+
+  Item* current = head_;
+  size_t count = 0;
+  while(current != NULL){
+    size_t listSize = current->last - current->first; // count the size of a single node
+  
+    if(loc < count + listSize){ // location found
+      return &current->val[current->first + (loc - count)];
+    }
+
+    count += listSize;
+    current = current->next; // location not found, proceed to next node
+  }
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
